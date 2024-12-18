f2f8657ab4989806eaf0df5bf3918283f0ec3e81
Rikaseptiana993 <162653329+Rikaseptiana993@users.noreply.github.com>
2024-12-18 12:05:25 +0700

    Add files via upload

---
+++ UTS/menu.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UTS;
+
+/**
+ *
+ * @author HP
+ */
+public class menu {
+    private String nama;
+    private int harga;
+    private int jumlah;
+
+    public menu(String nama, int harga, int jumlah) {
+        this.nama = nama;
+        this.harga = harga;
+        this.jumlah = jumlah;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public int hitungTotal() {
+        return harga * jumlah;
+    }
+}
