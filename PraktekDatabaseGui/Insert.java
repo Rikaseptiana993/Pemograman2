54c5ea024a58fa6416c7b755cfe2991598f19e71
Rikaseptiana993 <162653329+Rikaseptiana993@users.noreply.github.com>
2025-01-13 20:00:19 +0700

    Add files via upload

---
+++ PraktekDatabaseGui/Insert.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PraktekDatabaseGui;
+
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 
+ */
+public class Insert {
+
+    Connect conec = new Connect();
+    String prog;
+
+    public void insert(String nim, String nama, String alamat, String gender) {
+
+        try {
+            conec.koneksiData();
+
+            Statement statement = conec.conn.createStatement();
+            String sql = "insert into identitas values('" + nim + "','" + nama + "','" + alamat + "','" + gender + "');";
+
+            statement.executeUpdate(sql);
+            statement.close();
+
+            System.out.println("inserting query...");
+            prog = "Berhasil Menyimpan Kenangan Indah";
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+
+    }
+}
+
